1883c3601991c22fecdb912781377871b469a461
Md Hussain Musthaq S N <54637394+sanzder24@users.noreply.github.com>
2020-07-27 15:40:55 -0400

    Add files via upload

---
+++ build/precache-manifest.85daf2455ae28a4b42b438e90c241355.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea1fa17fe04c29a7e1a6600b3f7197e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cc5ec49fc24c4631aac",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "0250f45c474d4088869e",
+    "url": "/static/css/main.b5898a09.chunk.css"
+  },
+  {
+    "revision": "0cc5ec49fc24c4631aac",
+    "url": "/static/js/2.cfaf98ba.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.cfaf98ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0250f45c474d4088869e",
+    "url": "/static/js/main.337ee9bd.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
